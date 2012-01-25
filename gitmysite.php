4ff8bd4a0e18d5650a16eec6c917b86a2df3bdd6
Toby Allen <toby@toby.com>
2012-01-25 18:47:46 +0000

    Rearranged html and php

--- gitmysite.php
+++ gitmysite.php
@@ -9,151 +9,6 @@ $mySite->exec(@$_REQUEST['action'], $_REQUEST);
 Some interesting git resources
 http://www.draconianoverlord.com/2010/03/04/git-config.html
 */
-?>
-<html>
-	<head>
-		<title>Git my Site</title>
-		<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/themes/le-frog/jquery-ui.css" rel="stylesheet" type="text/css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/jquery-ui.min.js" type="text/javascript"></script>
-		<style type="text/css">
-<!--
-.sectionheader {
-	font-size: large;
-	font-weight: bold;
-	color: #000000;
-}
-.sectioncomments {
-	font-size: small;
-	font-weight: italic;
-	color: #ffffff;
-	width: 600px;
-
-}
-.header {
-	font-size: x-large;
-	font-weight: bold;
-	color: #CCCCCC;
-	
-}
-.results {
-	font-size: small;
-	color: #0099CC;
-	background-color: #CCCCCC;
-	border: 1px #660066;
-	width: 600px;
-}
--->
-		</style>
-		<script>
-	$(function() {
-	  var $tabs;
-		$tabs = $( "#tabs" ).tabs();
-
-	});
-		</script>
-	</head>
-	<body>
-		<div class="demo">
-			<div class="header ui-widget-content ">
-				<p>Git My Site <?php echo $_SERVER['SERVER_NAME']; ?></p>
-			</div>
-			<div id="tabs">
-				<ul>
-					<li><a href="#gitmysite_setup">Setup</a></li>
-					<li><a href="#gitmysite_commit">Add &amp; Commit</a></li>
-					<li><a href="#gitmysite_status">Status</a></li>
-					<li><a href="#gitmysite_results">Results</a></li>
-				</ul>
-				<div id="gitmysite_setup">
-					<div class="demo-description" style="display: none; ">
-						<p>Click tabs to swap between content that is broken into logical sections.</p>
-					</div>
-		
-					<p class="sectionheader">Create a New Repository in this Directory </p>
-					<p><a href="gitmysite.php?action=init">Create git Repository</a> 
-					- </p>
-					<p class="sectionheader">Secure the Directory</p>
-					<div class="sectioncomments">Provide a username and password to secure your .git directory.  You can also use this username and password when you clone your repo to your local machine.  
-					<strong>Note:</strong>  Clicking Submit will not overwrite any existing .htaccess and .htpasswd file you have in the .git folder. Please delete first or edit manually</div>
-					<form name="form2" method="post" action="gitmysite.php#gitmysite_results">
-						Username:
-						<input type="text" name="apache_username"> <BR>
-						Password:
-						<input type="password" name="apache_pass"><BR>
-						<input type=hidden value="apache_secure" name="action" >
-						<input type="submit" name="Submit2" value="Submit">
-					</form>
-					<form name="form1" method="get" action="gitmysite.php">
-						<p class="sectionheader">Set User Config Details</p>
-						<p>User Details:</p>
-						<p> Username:
-						<input type="text" name="username">
-						</p>
-						<p> Email:
-						<input type="text" name="email">
-						<input type=hidden value="username" name="action" >
-						<input type="submit" name="Submit" value="Submit">
-						</p>
-					</form>
-					<p class="sectionheader">Create and Edit Git Ignore File </p>
-					<?php if ($mySite->file_gitignore > '') {  ?>
-					<form action=gitmysite.php method=post>
-						<textarea name='content_gitignore' cols="40" rows="12"><?php echo $mySite->file_gitignore ; ?></textarea>
-						<input type=hidden  name="action" value="editgitignore">
-						<input type="submit" name="Submit" value="Submit">
-					</form>
-					<?php } 
-					else
-					{
-					?>
-					<p><a href="gitmysite.php?action=creategitignore">Create .gitignore</a> </p>
-					<p><a href="gitmysite.php?action=showgitignore">Edit .gitignore</a> </p>
-					<?php } ?>
-				</div>
-				<div id="gitmysite_commit">
-					<p>&nbsp; </p>
-					<p><a href="gitmysite.php?action=add">Add New &amp; Updated Files</a> - 
-					Commit Files
-					<form name="form1" method="get" action="gitmysite.php">
-						<p class="sectionheader">Commit Comment</p>
-						<textarea name="commit_comment" cols="80" rows="6">An Update
-						</textarea>
-						<input type=hidden  name="action" value="commit">
-						<input type="submit" name="Submit" value="Submit">
-						</p>
-					</form>
-				</div>
-				<div id="gitmysite_status">
-				</div>
-				<div  id="gitmysite_results">
-					- <a href="gitmysite.php?action=status#gitmysite_results">Status</a> -  <a href="gitmysite.php?action=log#gitmysite_results">Log</a> -</p>
-			
-					<p>Command Results </p>
-					 
-					<div class="results">
-				
-					<?php
-					if (isset($mySite) )
-					{ 
-						foreach ($mySite->gitOutput as $line)
-						{
-							echo "<BR>$line ";
-						}
-					}
-					?>	
-					</div>
-				
-				</div>
-			</div>
-		</div>
-
-		<p>&nbsp;</p>
-	</body>
-</html>
-
-
-<?php
 
 
 class gitmysite
@@ -374,3 +229,145 @@ require valid-user
 }
   
 ?>
+
+<html>
+	<head>
+		<title>Git my Site</title>
+		<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/themes/le-frog/jquery-ui.css" rel="stylesheet" type="text/css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/jquery-ui.min.js" type="text/javascript"></script>
+		<style type="text/css">
+<!--
+.sectionheader {
+	font-size: large;
+	font-weight: bold;
+	color: #000000;
+}
+.sectioncomments {
+	font-size: small;
+	font-weight: italic;
+	color: #ffffff;
+	width: 600px;
+
+}
+.header {
+	font-size: x-large;
+	font-weight: bold;
+	color: #CCCCCC;
+	
+}
+.results {
+	font-size: small;
+	color: #0099CC;
+	background-color: #CCCCCC;
+	border: 1px #660066;
+	width: 600px;
+}
+-->
+		</style>
+		<script>
+	$(function() {
+	  var $tabs;
+		$tabs = $( "#tabs" ).tabs();
+
+	});
+		</script>
+	</head>
+	<body>
+		<div class="demo">
+			<div class="header ui-widget-content ">
+				<p>Git My Site <?php echo $_SERVER['SERVER_NAME']; ?></p>
+			</div>
+			<div id="tabs">
+				<ul>
+					<li><a href="#gitmysite_setup">Setup</a></li>
+					<li><a href="#gitmysite_commit">Add &amp; Commit</a></li>
+					<li><a href="#gitmysite_status">Status</a></li>
+					<li><a href="#gitmysite_results">Results</a></li>
+				</ul>
+				<div id="gitmysite_setup">
+					<div class="demo-description" style="display: none; ">
+						<p>Click tabs to swap between content that is broken into logical sections.</p>
+					</div>
+		
+					<p class="sectionheader">Create a New Repository in this Directory </p>
+					<p><a href="gitmysite.php?action=init">Create git Repository</a> 
+					- </p>
+					<p class="sectionheader">Secure the Directory</p>
+					<div class="sectioncomments">Provide a username and password to secure your .git directory.  You can also use this username and password when you clone your repo to your local machine.  
+					<strong>Note:</strong>  Clicking Submit will not overwrite any existing .htaccess and .htpasswd file you have in the .git folder. Please delete first or edit manually</div>
+					<form name="form2" method="post" action="gitmysite.php#gitmysite_results">
+						Username:
+						<input type="text" name="apache_username"> <BR>
+						Password:
+						<input type="password" name="apache_pass"><BR>
+						<input type=hidden value="apache_secure" name="action" >
+						<input type="submit" name="Submit2" value="Submit">
+					</form>
+					<form name="form1" method="get" action="gitmysite.php">
+						<p class="sectionheader">Set User Config Details</p>
+						<p>User Details:</p>
+						<p> Username:
+						<input type="text" name="username">
+						</p>
+						<p> Email:
+						<input type="text" name="email">
+						<input type=hidden value="username" name="action" >
+						<input type="submit" name="Submit" value="Submit">
+						</p>
+					</form>
+					<p class="sectionheader">Create and Edit Git Ignore File </p>
+					<?php if ($mySite->file_gitignore > '') {  ?>
+					<form action=gitmysite.php method=post>
+						<textarea name='content_gitignore' cols="40" rows="12"><?php echo $mySite->file_gitignore ; ?></textarea>
+						<input type=hidden  name="action" value="editgitignore">
+						<input type="submit" name="Submit" value="Submit">
+					</form>
+					<?php } 
+					else
+					{
+					?>
+					<p><a href="gitmysite.php?action=creategitignore">Create .gitignore</a> </p>
+					<p><a href="gitmysite.php?action=showgitignore">Edit .gitignore</a> </p>
+					<?php } ?>
+				</div>
+				<div id="gitmysite_commit">
+					<p>&nbsp; </p>
+					<p><a href="gitmysite.php?action=add">Add New &amp; Updated Files</a> - 
+					Commit Files
+					<form name="form1" method="get" action="gitmysite.php">
+						<p class="sectionheader">Commit Comment</p>
+						<textarea name="commit_comment" cols="80" rows="6">An Update
+						</textarea>
+						<input type=hidden  name="action" value="commit">
+						<input type="submit" name="Submit" value="Submit">
+						</p>
+					</form>
+				</div>
+				<div id="gitmysite_status">
+				</div>
+				<div  id="gitmysite_results">
+					- <a href="gitmysite.php?action=status#gitmysite_results">Status</a> -  <a href="gitmysite.php?action=log#gitmysite_results">Log</a> -</p>
+			
+					<p>Command Results </p>
+					 
+					<div class="results">
+				
+					<?php
+					if (isset($mySite) )
+					{ 
+						foreach ($mySite->gitOutput as $line)
+						{
+							echo "<BR>$line ";
+						}
+					}
+					?>	
+					</div>
+				
+				</div>
+			</div>
+		</div>
+
+		<p>&nbsp;</p>
+	</body>
+</html>
